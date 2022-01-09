29bfcb0d5dc8bba9d8a1a8713bf0f425e523c8b1
Alice Coelho <alicecomoura@outlook.com>
2022-01-08 23:25:33 -0300

    Create read, update and delete functions

--- script.js
+++ script.js
@@ -9,14 +9,28 @@ const closeModal = () => document.getElementById('modal')
 // CRUD -create read update delete
 
 const tempClient = {  //tempory client
-    name: 'Alice',
-    email: 'alice@gmail.com',
+    name: 'Rafa',
+    email: 'rafa@gmail.com',
     celphone: '125488759',
     city: 'Rio de Janeiro'
 }
 
 const getLocalStorage = () => JSON.parse(localStorage.getItem('database_client')) ?? []
 const setLocalStorage = (databaseClient) => localStorage.setItem('database_client', JSON.stringify(databaseClient))
+
+const deleteClient = (index) => {
+    const databaseClient = readClient()
+    databaseClient.splice(index, 1)
+    setLocalStorage(databaseClient)
+}
+
+const updateClient = (index, client) => {
+    const dataBaseClient = readClient()
+    dataBaseClient[index] = client
+    setLocalStorage(dataBaseClient)
+}
+
+const readClient = () => getLocalStorage()
 
 const createClient = (client) => {
     const databaseClient = getLocalStorage()
